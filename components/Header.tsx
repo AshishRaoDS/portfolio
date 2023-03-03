4fe668ce49d510d28dd497400db4b4877eaba28c
AshishRaoDS <ashraods@gmail.com>
2023-03-03 19:00:30 +0530

    fix: header changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import styles from "../styles/Header.module.scss"
+import Poppins from "@next/font/google"
 
 type Props = {}
+
+// const poppins = Poppins({
+//   weight: []
+// })
 
 const Header = (props: Props) => {
   return (
     <div className={styles.headerContainer}>
-      <h2 className={styles.name}>Ashish Rao</h2>
-      <div className={styles.navContainer}>
-        <button>Home</button>
-        <button>About</button>
-        <button>Projects</button>
-        <button>Contact</button>
-      </div>
+      {/* <h2 className={styles.name}>Ashish Rao</h2> */}
+      <nav className={styles.navContainer}>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Projects</a>
+        <a href="#">Contact</a>
+      </nav>
 
     </div>
   )
